b8442b66b7814660df6e9c90cc258e873ac53a4c
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2019-01-18 16:49:15 +0700

    Simplify Template

--- sts-web/src/main/java/com/wissensalt/rnd/sts/web/controller/IndexController.java
+++ sts-web/src/main/java/com/wissensalt/rnd/sts/web/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.wissensalt.rnd.sts.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -11,8 +13,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    @Value("${web.head.title}")
+    private String webTitle;
+
+    @Value("${web.head.subtitle}")
+    private String webSubtitle;
+
+    @Value("${web.footer.version}")
+    private String version;
+
+    @Value("${web.footer.year-range}")
+    private String yearRange;
+
+    @Value("${web.footer.link}")
+    private String link;
+
+    @Value("${web.footer.link-text}")
+    private String linkText;
+
     @GetMapping("/")
-    public String showIndex() {
+    public String showIndex(Model p_Model) {
+        p_Model.addAttribute("webTitle", webTitle);
+        p_Model.addAttribute("webSubtitle", webSubtitle);
+        p_Model.addAttribute("version", version);
+        p_Model.addAttribute("yearRange", yearRange);
+        p_Model.addAttribute("link", link);
+        p_Model.addAttribute("linkText", linkText);
         return "index";
     }
 }
